2cdec621650144cffce3653f8d1fdbbca3d479de
Joy Sarker <joysarker39@gmail.com>
2022-01-02 23:31:51 +0600

    Update ractangle.cpp

--- ractangle.cpp
+++ ractangle.cpp
@@ -2,9 +2,9 @@ void ractangle(int x, int y, int xx, int yy){
     //drawLineDDA(x,y, x+xx, y);
     clipLine(winMin, winMax, {x,y},{x+xx,y} );
     //drawLineDDA(x+xx,y, x+xx, y+yy);
-    clipLine(winMin, winMax, {x,y},{x+xx,y+yy} );
+    clipLine(winMin, winMax, {x+xx,y},{x+xx,y+yy} );
     //drawLineDDA(x,y+yy, x+xx, y+yy);
-    clipLine(winMin, winMax, {x,y+yy},{x+xx,y+yy} );  
+    clipLine(winMin, winMax, {x,y+yy},{x+xx,y+yy} );
     //drawLineDDA(x,y+yy, x, y);
     clipLine(winMin, winMax, {x,y+yy},{x,y} );
 }
